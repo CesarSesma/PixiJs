fddb5ef0f4d6030f7dccb81f831f3ba380b6c155
Cesar Sesma <cesarsesma@gmail.com>
2022-03-27 16:05:12 -0300

    animated

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,4 +1,4 @@
-import { Container } from "pixi.js";
+import { AnimatedSprite, Container, Texture } from "pixi.js";
 import { GokuKame } from "./GokuKame";
 
 export class Scene extends Container {
@@ -6,6 +6,38 @@ export class Scene extends Container {
         super();
         const gokuPoder: GokuKame = new GokuKame();
         this.addChild(gokuPoder);
+
+        const vegetaAnimated: AnimatedSprite = new AnimatedSprite
+        ([
+            Texture.from("Vegeta0"),
+            Texture.from("Vegeta1"),
+            Texture.from("Vegeta2"),
+            Texture.from("Vegeta3"),
+            Texture.from("Vegeta4"),
+            Texture.from("Vegeta5"),
+            Texture.from("Vegeta6"),
+            Texture.from("Vegeta7"),
+            Texture.from("Vegeta8"),
+            Texture.from("Vegeta9"),
+            Texture.from("Vegeta10"),
+            Texture.from("Vegeta11"),
+            Texture.from("Vegeta12"),
+            Texture.from("Vegeta13"),
+            Texture.from("Vegeta14"),
+            Texture.from("Vegeta15"),
+            Texture.from("Vegeta16"),
+            Texture.from("Vegeta17"),
+            Texture.from("Vegeta18"),
+            Texture.from("Vegeta19"),
+            Texture.from("Vegeta20"),
+            Texture.from("Vegeta21"),
+            Texture.from("Vegeta22"),
+            Texture.from("Vegeta23")
+        ],true);
+        vegetaAnimated.play();
+        vegetaAnimated.animationSpeed = 0.2;
+        this.addChild(vegetaAnimated);
+
     }
 
 }
--- src/assets.ts
+++ src/assets.ts
@@ -1,7 +1,32 @@
 export const assets = [
     {url:"./goku.png", name:"Goku"},
     {url:"./nube.png", name:"Nube"},
-    {url:"./kamehameOri.png", name:"Kame"}
+    {url:"./kamehameOri.png", name:"Kame"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-0.png", name:"Vegeta0"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-1.png", name:"Vegeta1"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-3.png", name:"Vegeta3"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-4.png", name:"Vegeta4"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-2.png", name:"Vegeta2"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-5.png", name:"Vegeta5"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-6.png", name:"Vegeta6"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-7.png", name:"Vegeta7"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-8.png", name:"Vegeta8"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-9.png", name:"Vegeta9"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-10.png", name:"Vegeta10"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-11.png", name:"Vegeta11"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-12.png", name:"Vegeta12"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-13.png", name:"Vegeta13"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-14.png", name:"Vegeta14"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-15.png", name:"Vegeta15"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-16.png", name:"Vegeta16"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-17.png", name:"Vegeta17"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-18.png", name:"Vegeta18"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-19.png", name:"Vegeta19"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-20.png", name:"Vegeta20"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-21.png", name:"Vegeta21"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-22.png", name:"Vegeta22"},
+    {url:"./Dragon-Ball-Super-4-0/Dragon-Ball-Super-4-23.png", name:"Vegeta23"}
+
 ];
 
 
